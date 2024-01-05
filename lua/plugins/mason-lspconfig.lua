73ffc62007c2e48f5aab30f76018d18ba2f2b8b1
Huayu Zhang <zhanghuayu.dev@gmail.com>
2024-01-06 01:12:22 +0800

    remove some lsp that not allowed

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,38 +1,30 @@
 return {
-	{ -- https://github.com/williamboman/mason-lspconfig.nvim
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"rust_analyzer",
-					"clangd",
-					"cmake",
-					"neocmake",
-					"dockerls",
-					"docker_compose_language_service",
-					"golangci_lint_ls",
-					"gopls",
-					"java_language_server",
-					"ltex",
-					"marksman",
-					"opencl_ls",
-					"pyright",
-					"sqlls",
-					"lemminx",
-					"yamlls",
-					"lua_ls",
-					"shfmt",
-					"stylua",
-					"html-lsp",
-					"json-lsp",
-					"fixjson",
-					"prettier",
-				},
-			})
-		end,
-	},
+    { -- https://github.com/williamboman/mason-lspconfig.nvim
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        opts = {
+            auto_install = true,
+        },
+        config = function()
+            require("mason-lspconfig").setup {
+                ensure_installed = {
+                    "rust_analyzer",
+                    "clangd",
+                    "cmake",
+                    "neocmake",
+                    "dockerls",
+                    "docker_compose_language_service",
+                    "golangci_lint_ls",
+                    "gopls",
+                    "java_language_server",
+                    "ltex",
+                    "marksman",
+                    "opencl_ls",
+                    "pyright",
+                    "sqlls",
+                    "lua_ls",
+                },
+            }
+        end,
+    },
 }
