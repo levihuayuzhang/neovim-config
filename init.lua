9f306708152ef469e0b97cc99cb24cbd6d5e02db
Huayu Zhang <zhanghuayu.dev@gmail.com>
2024-01-05 22:12:26 +0800

    add logo

--- init.lua
+++ init.lua
@@ -13,16 +13,16 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- open neotree on start up
-vim.api.nvim_create_augroup("neotree", {})
-vim.api.nvim_create_autocmd("UiEnter", {
-	desc = "Open Neotree automatically",
-	group = "neotree",
-	callback = function()
-		if vim.fn.argc() == 0 then
-			vim.cmd "Neotree toggle"
-		end
-	end,
-})
+-- vim.api.nvim_create_augroup("neotree", {})
+-- vim.api.nvim_create_autocmd("UiEnter", {
+-- 	desc = "Open Neotree automatically",
+-- 	group = "neotree",
+-- 	callback = function()
+-- 		if vim.fn.argc() == 0 then
+-- 			vim.cmd "Neotree toggle"
+-- 		end
+-- 	end,
+-- })
 
 require("configs.options")
 require("configs.keymaps")
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,18 @@ return {
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
             local builtin = require("telescope.builtin")
+            local actions = require("telescope.actions")
+            local trouble = require("trouble.providers.telescope")
+            local telescope = require("telescope")
+            telescope.setup {
+                defaults = {
+                    mappings = {
+                        i = { ["<c-t>"] = trouble.open_with_trouble },
+                        n = { ["<c-t>"] = trouble.open_with_trouble },
+                    },
+                },
+            }
+
             vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         end
