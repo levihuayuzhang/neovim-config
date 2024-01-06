614db8d8ea2ebe1dc8b5c19092ae89db7a0fd45d
Huayu Zhang <zhanghuayu.dev@gmail.com>
2024-01-06 13:33:26 +0800

    new transparent color scheme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,15 +2,16 @@
 
 return {
     {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
-            require('lualine').setup {
+            require("lualine").setup {
                 options = {
                     icons_enabled = true,
-                    theme = 'auto',
-                    section_separators = { left = '', right = '' },
-                    component_separators = { left = '', right = '' },
+                    theme = "auto",
+                    -- theme = 'tokyonight',
+                    section_separators = { left = "", right = "" },
+                    component_separators = { left = "", right = "" },
                     disabled_filetypes = {
                         statusline = {},
                         winbar = {},
@@ -22,29 +23,29 @@ return {
                         statusline = 1000,
                         tabline = 1000,
                         winbar = 1000,
-                    }
+                    },
                 },
                 sections = {
-                    lualine_a = { 'mode' },
-                    lualine_b = { 'branch', 'diff', 'diagnostics' },
-                    lualine_c = { { 'filename', path = 3 } },
-                    lualine_x = { 'encoding', 'fileformat', 'filetype' },
-                    lualine_y = { 'progress' },
-                    lualine_z = { 'location' }
+                    lualine_a = { "mode" },
+                    lualine_b = { "branch", "diff", "diagnostics" },
+                    lualine_c = { { "filename", path = 3 } },
+                    lualine_x = { "encoding", "fileformat", "filetype" },
+                    lualine_y = { "progress" },
+                    lualine_z = { "location" },
                 },
                 inactive_sections = {
                     lualine_a = {},
                     lualine_b = {},
-                    lualine_c = { { 'filename', path = 3 } },
-                    lualine_x = { 'location' },
+                    lualine_c = { { "filename", path = 3 } },
+                    lualine_x = { "location" },
                     lualine_y = {},
-                    lualine_z = {}
+                    lualine_z = {},
                 },
                 tabline = {},
                 winbar = {},
                 inactive_winbar = {},
-                extensions = {}
+                extensions = {},
             }
-        end
-    }
+        end,
+    },
 }
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -11,7 +11,7 @@ return {
             "3rd/image.nvim",
         },
         config = function()
-            vim.keymap.set('n', '<leader>e', '<cmd>Neotree filesystem reveal left <CR>')
-        end
-    }
+            vim.keymap.set("n", "<leader>e", "<cmd>Neotree filesystem reveal left <CR>")
+        end,
+    },
 }
